1b554f0a10fb7553263a945dcfff4557119bd37d
Naveen Bavu <naveen.bavu@albertsons.com>
2019-10-15 17:56:15 -0700

    hard coded for 3116 store

--- src/main/java/com/ecommerce/albertsons/repository/StoreItemRepository.java
+++ src/main/java/com/ecommerce/albertsons/repository/StoreItemRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 public interface StoreItemRepository extends MongoRepository<StoreItemModel, String> {
-  
-  @Query("{'corporateItemCd':{ '$in':?0}}")
+
+  @Query("{'upcId':{ '$in':?0} , 'storeId': 3116}")
   List<StoreItemModel> findByCorporateItemCd(List<String> cicList);
 }
